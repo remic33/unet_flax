3c18e90c4b41d3e15815be1f0dcc30d3d0a602a1
Remi Caland <caland.remi@gmail.com>
2022-07-27 17:39:17 +0200

    feat: corrected train implementation, need test with correct dataloaders

--- src/train.py
+++ src/train.py
@@ -4,6 +4,12 @@ from flax import linen as nn
 import jax
 from src.model import BasicUnet
 import numpy as np
+import optax
+from flax.training import train_state
+from dataclasses import dataclass
+
+img_size = (256, 256, 1)
+batch_size = 6
 
 @jax.jit
 def train_step(state, imgs, gt_labels):
@@ -22,7 +28,7 @@ def train_step(state, imgs, gt_labels):
 
 @jax.jit
 def eval_step(state, imgs, gt_labels):
-    logits = BasicUnet().apply({'params': state.params}, imgs)
+    logits = BasicUnet(training=False).apply({'params': state.params}, imgs)
     return compute_metrics(logits=logits, gt_labels=gt_labels)
 
 
@@ -52,19 +58,27 @@ def evaluate_model(state, test_imgs, test_lbls):
     return metrics
 
 
-def create_train_state(key, learning_rate, momentum):
-    cnn = CNN()
-    params = cnn.init(key, jnp.ones([1, *mnist_img_size]))['params']
-    sgd_opt = optax.sgd(learning_rate, momentum)
+def create_train_state(keys, learning_rate, momentum):
+    unet = BasicUnet(training=False)
+    key1, key2, keys3 = keys
+    init_variables = unet.init(key1, jnp.ones([1, *img_size]))
+
+    params = unet.apply(init_variables, jnp.ones([1, *img_size]), rngs={'params': key1, 'dropout': key2})
+    # unet.training = True
+    # params = unet['params']
+    sgd_opt = optax.adam(learning_rate, momentum)
     # TrainState is a simple built-in wrapper class that makes things a bit cleaner
-    return train_state.TrainState.create(apply_fn=cnn.apply, params=params, tx=sgd_opt)
+    return train_state.TrainState.create(apply_fn=unet.apply, params=params, tx=sgd_opt)
 
 
-def compute_metrics(*, logits, gt_labels):
-    one_hot_gt_labels = jax.nn.one_hot(gt_labels, num_classes=10)
+@jax.jit
+def binary_cross_entropy(y_hat, y):
+    return jnp.mean(jnp.maximum(y_hat, 0) - y_hat * y + jnp.log1p(jnp.exp(-jnp.abs(y_hat))))
 
-    loss = -jnp.mean(jnp.sum(one_hot_gt_labels * logits, axis=-1))
-    accuracy = jnp.mean(jnp.argmax(logits, -1) == gt_labels)
+
+def compute_metrics(*, logits, y_true):
+    loss = binary_cross_entropy(logits, y_true)
+    accuracy = jnp.mean(logits == y_true)
 
     metrics = {
         'loss': loss,
@@ -73,16 +87,17 @@ def compute_metrics(*, logits, gt_labels):
     return metrics
 
 
-class Train(nn.module):
+@dataclass
+class Train:
     seed: int = 0
-    learning_rate: float = 0.1
+    learning_rate: float = 1e-4
     momentum: float = 0.9
     num_epochs: int = 2
     batch_size: int = 32
 
-    @nn.compact
-    def __call__(self, epochs):
-        train_state = create_train_state(jax.random.PRNGKey(self.seed), self.learning_rate,
+    def run(self, train_loader, test_images, test_lbls):
+        key1, key2, key3 = jax.random.split(jax.random.PRNGKey(42), 3)
+        train_state = create_train_state((key1, key2, key3), self.learning_rate,
                                          self.momentum)
 
         for epoch in range(1, self.num_epochs + 1):
